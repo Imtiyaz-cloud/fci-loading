995a339254b8337cb0680e8479e8551d6409f956
Imtiyaz-cloud <asmeenkhatoon000786@gmail.com>
2026-02-09 00:06:02 +0530

    Update App.py

--- App.py
+++ App.py
@@ -17,8 +17,10 @@ st.dataframe(df)
 
 # Data save karne ke liye (Sample button)
 if st.button("Data Update Karein"): 
+  
 conn.update(spreadsheet=url, data=df) 
 st.success("Data safalta purvak save ho gaya!")
+
 
 
 
