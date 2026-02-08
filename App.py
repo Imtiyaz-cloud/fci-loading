345c1469d0a29a966126d01e38cd16e26d9b5548
Imtiyaz-cloud <asmeenkhatoon000786@gmail.com>
2026-02-09 00:00:59 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,18 +2,19 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
-1. Google Sheet se connect karne ke liye
+# Google Sheet se connect karne ke liye
 conn = st.connection("gsheets", type=GSheetsConnection, credentials="google_keys.json")
 
-2. Aapki sheet ka link
+# Aapki sheet ka link
 url = ""
 
-3. Sheet se data uthane ke liye
+# sheet se data uthane ke liye
 df = conn.read(spreadsheet=url)
 
-Screen par dikhane ke liye
+#Screen par dikhane ke liye
 st.title("FCI Online Data") st.dataframe(df)
 
-4. Data save karne ke liye (Sample button)
+# Data save karne ke liye (Sample button)
 if st.button("Data Update Karein"): conn.update(spreadsheet=url, data=df) st.success("Data safalta purvak save ho gaya!")
+
 
